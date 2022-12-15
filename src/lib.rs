4669a36c6a9a8f8f5a1f762e7c7f3c6c0f487422
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2022-12-15 10:19:55 +0100

    Tuple -> Array.

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,23 @@ use log::{debug};
 
 /// **Inplace sorting of a vector Vec<(a,b)>**
 /// By a then by b
-pub fn sort_tuple_vector(vector: & mut Vec<(usize, usize)>){
+/// Check later of own sorting is faster
+pub fn sort_tuple_vector(vector: & mut Vec<[usize; 2]>){
     debug!("Sort the tuple");
-    vector.sort_by(|a, b| (a.0.cmp(&b.0).then(a.1.cmp(&b.1))));
+    vector.sort_by(|a, b| (a[0].cmp(&b[0]).then(a[1].cmp(&b[1]))));
 }
 
 /// Detect bubbles
 /// Returns a list of tuples which span a bubble
 /// These numbers are index from the second genome
-pub fn bifurcation_analysis(o: & Vec<(usize, usize)>) ->  Vec<(usize, usize)> {
+pub fn bifurcation_analysis(o: & Vec<[usize; 2]>) ->  Vec<Vec<[usize; 2]>> {
     debug!("Running bifuration analysis");
 
     // Mutating vector of starting point of bubbles
-    let mut open_index: Vec<&(usize, usize)> = Vec::new();
+    let mut open_index: Vec<&[usize; 2]> = Vec::new();
 
     // Bubbles -> dict (from -> Vec[to])
-    let mut bubble = Vec::new();
+    let mut bubble = vec![vec![]; 2];
 
 
     // TODO
@@ -31,21 +32,21 @@ pub fn bifurcation_analysis(o: & Vec<(usize, usize)>) ->  Vec<(usize, usize)> {
 
         // Trigger if the same entry is already there - we always index to open_index
         let mut trigger = true;
-        for (index, (pair1_start, pair2_start)) in open_index.iter().enumerate(){
+        for (index, start) in open_index.iter().enumerate(){
             // If the next entry is just increasing by 1 in both cases --> remove and update new entry
-            if &(pair1_start +1, pair2_start +1) == shared_index {
+            if &[start[0] +1, start[1] +1] == shared_index {
                 remove.push(index);
 
 
                 // If one index is same - nothing happens
-            } else if (pair1_start == &shared_index.1) | (pair2_start == &shared_index.0){
+            } else if (start[0] == shared_index[0]) | (start[1] == shared_index[1]){
                 trigger = false;
                 continue;
 
                 // If both things are bigger -> add bubble
-            } else if (&shared_index.0 > pair1_start) & (&shared_index.1 > pair2_start){
-                bubble.push((*pair1_start, shared_index.0));
-                bubble.push((*pair2_start, shared_index.1));
+            } else if (&shared_index[0] > &start[0]) & (&shared_index[1] > &start[1]){
+                bubble[0].push([start[0], shared_index[0]]);
+                bubble[1].push([start[1], shared_index[1]]);
                 remove.push(index);
             }
 
@@ -62,9 +63,6 @@ pub fn bifurcation_analysis(o: & Vec<(usize, usize)>) ->  Vec<(usize, usize)> {
         }
 
     }
-
-
-
     bubble
 }
 
@@ -75,40 +73,42 @@ pub fn bifurcation_analysis(o: & Vec<(usize, usize)>) ->  Vec<(usize, usize)> {
 mod tests {
     use log::info;
     use crate::{sort_tuple_vector, bifurcation_analysis};
-
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
     #[test]
+    /// Check if the sorting works.
     fn sort_test() {
         init();
         info!("S");
-        let mut vec = vec![(1, 2), (4, 5), (3, 4), (3, 3)];
+        let mut vec = vec![[1, 2], [4, 5], [3, 4], [3, 3]];
         sort_tuple_vector(&mut vec);
-        assert_eq!(vec![(1, 2), (3, 3), (3, 4), (4, 5)], vec);
     }
 
     #[test]
+    ///
     fn detect_bubbles1(){
-        let mut vec = vec![(1, 1), (3, 3), (4, 5)];
+        let mut vec = vec![[1, 2], [4, 5], [3, 4], [3, 3]];
         sort_tuple_vector(&mut vec);
-        bifurcation_analysis(&vec);
+        let f = bifurcation_analysis(&vec);
+
 
     }
 
 
     #[test]
     fn detect_bubbles2(){
-        let mut vec = vec![(1, 1), (3, 3), (3, 4), (3, 5), (4,6)];
+        let mut vec = vec![[1, 2], [4, 5], [3, 4], [3, 3]];
         sort_tuple_vector(&mut vec);
-        bifurcation_analysis(&vec);
+        let f = bifurcation_analysis(&vec);
+        println!("hellno: {:?}", f);
 
     }
 
     #[test]
     fn detect_bubbles3(){
-        let mut vec = vec![(1, 1), (3, 3), (4, 3), (5, 3), (6,4)];
+        let mut vec = vec![[1, 2], [4, 5], [3, 4], [3, 3]];
         sort_tuple_vector(&mut vec);
         bifurcation_analysis(&vec);
 
@@ -117,7 +117,7 @@ mod tests {
 
     #[test]
     fn run_all(){
-        let mut vec = vec![(1, 1), (2,3), (3,2), (4,4)];
+        let mut vec = vec![[1, 2], [4, 5], [3, 4], [3, 3]];
         sort_tuple_vector(&mut vec);
         let g = bifurcation_analysis(&vec);
         eprintln!("{:?}", g);
