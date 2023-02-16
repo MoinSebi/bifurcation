5dd41c7fc04fc110dede31a561f29e639f3cfa64
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-02-16 17:44:43 +0100

    Update bench file.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,16 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use bifurcation::bifurcation_analysis_meta;
 
-pub fn make_index() -> Vec<[u32; 3]> {
+
+/// Creates data which should reflect real graph data
+pub fn data_creation() -> Vec<[u32; 3]> {
     let mut mm = Vec::new();
     mm.push([1,1000000,2]);
     for x in 0..1000000{
-        if x%2 == 0{
-            mm.push([x,x+5,10])
+        if x%20 == 0{
+            mm.push([x,x+500,10])
         } else if x%5 == 0{
-            mm.push([x, x-1,10])
+            mm.push([x+3, x+40000,10])
         } else {
-            mm.push([x, x+2,10])
+            mm.push([x+1, x+1,10])
 
         }
     }
@@ -18,17 +20,23 @@ pub fn make_index() -> Vec<[u32; 3]> {
     mm
 }
 
-pub fn test(input: &Vec<[u32; 3]>){
-    let ff = bifurcation_analysis_meta(&input);
 
+/// This bench runs the bifurcation_analysis_meta function from lib
+pub fn bench_bifurcation_analysis_meta(input: &Vec<[u32; 3]>){
+    bifurcation_analysis_meta(&input);
 }
 
+
+/// Bench with criterion
+/// I seems like a 1_000_000 vector takes ~ 5 ms on my setting.
 fn criterion_benchmark(c: &mut Criterion) {
-    let data = make_index();
-    c.bench_function("faster network4", |b| b.iter(|| test(&data)));
+    let data = data_creation();
+    println!("The data is of length {}", data.len());
+    c.bench_function("faster network4", |b| b.iter(|| bench_bifurcation_analysis_meta(&data)));
 
 
 }
 
+// Run the bench
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
